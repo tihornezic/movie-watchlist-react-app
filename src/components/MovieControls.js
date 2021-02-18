6b0901b9ae2f7767d4d75cb212b0839aa3c8acba
Tihor Nezic <tihor.nezic@gmail.com>
2021-02-18 10:43:47 +0100

    remove-movie-from-watchlist

--- src/components/MovieControls.js
+++ src/components/MovieControls.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import {useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState'
 
 const MovieControls = ({movie, type}) => {
+    const {removeMovieFromWatchlist} = useContext(GlobalContext)
+
     return (
         <div className='inner-card-controls'>
-            <button className='ctrl-btn'>
-                <i className='fa-fw far fa-eye'></i>
-            </button>
+            {type === 'watchlist' && (
+                <>
+                    <button className='ctrl-btn'>
+                        <i className='fa-fw far fa-eye'></i>
+                    </button>
 
-            <button className='ctrl-btn'>
-                <i className='fa-fw fa fa-times'></i>
-            </button>
+                    <button className='ctrl-btn' onClick={() => removeMovieFromWatchlist(movie.id)}>
+                        <i className='fa-fw fa fa-times'></i>
+                    </button>
+                </>
+            )}
         </div>
     )
 }
